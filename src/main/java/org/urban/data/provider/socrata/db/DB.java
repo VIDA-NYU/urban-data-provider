a9a4cdc47cc2cbf5eb20c8eefeb86290bf564572
Heiko Mueller <heiko.muller@gmail.com>
2019-10-04 22:26:25 -0400

    Fix issue with trimming of values

--- src/main/java/org/urban/data/provider/socrata/db/DB.java
+++ src/main/java/org/urban/data/provider/socrata/db/DB.java
@@ -356,7 +356,7 @@ public class DB {
                 CSVFormat.TDF
                         .withFirstRecordAsHeader()
                         .withIgnoreHeaderCase()
-                        .withTrim()
+                        .withIgnoreSurroundingSpaces(false)
         );
     }
     
